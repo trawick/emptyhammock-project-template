192dc2220ee3bea227161d7b2498fd820773c752
Jeff Trawick <trawick@gmail.com>
2021-03-06 09:47:30 -0500

    Check for missing migrations when running tests.

--- run_tests.sh
+++ run_tests.sh
@@ -20,3 +20,9 @@ if ! coverage report --fail-under ${MIN_COVERAGE}; then
     echo 'FAILED!' 1>&2
     exit 1
 fi
+
+# Fail if any migrations need to be generated.
+if ! python manage.py makemigrations --dry-run --noinput --settings=${PROJECT}.settings.dev | grep -q 'No changes detected'; then
+    echo 'Migrations need to be generated.  Run "./manage.py makemigrations --dry-run"' 1>&2
+    exit 1
+fi
